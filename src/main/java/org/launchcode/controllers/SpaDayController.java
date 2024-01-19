2a203b67728a5ca153a06db7b24a93d438a6f7de
stephenmdevine <stephenmdevine@gmail.com>
2024-01-19 17:36:24 -0600

    Studio complete

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -24,30 +24,17 @@ public class SpaDayController {
         }
     }
 
-    @GetMapping(value="")
-    @ResponseBody
+    @GetMapping("form")
     public String customerForm () {
-        String html = "<form method = 'post'>" +
-                "Name: <br>" +
-                "<input type = 'text' name = 'name'>" +
-                "<br>Skin type: <br>" +
-                "<select name = 'skintype'>" +
-                "<option value = 'oily'>Oily</option>" +
-                "<option value = 'combination'>Combination</option>" +
-                "<option value = 'normal'>Normal</option>" +
-                "<option value = 'dry'>Dry</option>" +
-                "</select><br>" +
-                "Manicure or Pedicure? <br>" +
-                "<select name = 'manipedi'>" +
-                "<option value = 'manicure'>Manicure</option>" +
-                "<option value = 'pedicure'>Pedicure</option>" +
-                "</select><br>" +
-                "<input type = 'submit' value = 'Submit'>" +
-                "</form>";
-        return html;
+        return "form";
     }
 
-    @PostMapping(value="")
+    @GetMapping("menu")
+    public String displayMenu() {
+        return "menu";
+    }
+
+    @PostMapping("menu")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
         ArrayList<String> facials = new ArrayList<>();
